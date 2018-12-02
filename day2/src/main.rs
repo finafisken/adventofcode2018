b865794466cf431aec9b5a03d2630abeaad746c4
Markus Warne <markus.warne@gmail.com>
2018-12-02 20:28:05 +0100

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashSet;
 
-
 fn main() {
     let mut file = File::open("input.txt").unwrap();
     let mut input = String::new();
@@ -43,30 +42,36 @@ fn part1(input: &String) -> u32 {
             if seen_three && seen_two {
                 break;
             }
-            println!("{:?} present {:?} times", u_letter, count);
         }
     }
     twos * threes
 }
 
-fn part2(input: &String) -> i32 {
-    2
+fn part2(input: &String) -> String {
+    let lines: Vec<&str> = input.lines().collect();
+    let str_length = lines.first().unwrap().chars().count();
+    for idx in 1..str_length {
+        let mut strings_without_idx_char = HashSet::new();
+        for line in &lines {
+            let val: String = line.chars().take(idx-1).chain(line.chars().skip(idx)).collect();
+            if !strings_without_idx_char.insert(val.clone()) {
+                return val
+            }
+        }
+    }
+    "".to_string()
 }
 
 #[test]
 fn validate_part1() {
-    let input = "abcdef\n
-        bababc\n
-        abbcde\n
-        abcccd\n
-        aabcdd\n
-        abcdee\n
-        ababab".to_string();
+    let input = "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab".to_string();
 
     assert_eq!(12, part1(&input));
 }
 
-// #[test]
-// fn validate_part2() {
-//     assert_eq!(0, part2(&"+1\n-1".to_string()));
-// }
+ #[test]
+ fn validate_part2() {
+    let input = "abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz".to_string();
+
+    assert_eq!("fgij", part2(&input));
+}
