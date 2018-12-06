b6f2a1381821685d386a49e4c0787538424d8881
Markus Warne <markus.warne@gmail.com>
2018-12-06 23:22:10 +0100

    some refactor

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -8,23 +8,29 @@ fn main() {
     file.read_to_string(&mut input).unwrap();
 
     println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(&input, 10000));
 }
 
 // figure out board size (max/min x/y tot)
 // iterate over all points in board, calc manhattan distance to each point
 // assign point to closest coord
 
-fn part1(input: &String) -> i32 {
+fn prepare(input: &String) -> (Vec<(i32, i32)>, (i32, i32), (i32, i32)) {
     let coordinates: Vec<(i32, i32)> = input.lines().map(|p| {
         let p_vec: Vec<&str> = p.split(", ").collect();
         (p_vec[0].parse().unwrap(), p_vec[1].parse().unwrap())
     }).collect();
 
     let (max_x, max_y) = coordinates.iter().fold((0, 0), |(max_x, max_y), (x, y)| (cmp::max(max_x, *x), cmp::max(max_y, *y)));
-    let (min_x, min_y) = coordinates.iter().fold((400, 400), |(min_x, min_y), (x, y)| (cmp::min(min_x, *x), cmp::min(min_y, *y)));
+    let (min_x, min_y) = coordinates.iter().fold((i32::max_value(), i32::max_value()), |(min_x, min_y), (x, y)| (cmp::min(min_x, *x), cmp::min(min_y, *y)));
 
+    (coordinates, (max_x, max_y), (min_x, min_y))
+}
+
+fn part1(input: &String) -> i32 {
+    let (coordinates, (max_x, max_y), (min_x, min_y)) = prepare(input);
     let mut coord_areas = vec![0; coordinates.len()];
+
     for x in min_x..max_x {
         for y in min_y..max_y {
             // owned by point {index} and closest distance
@@ -48,19 +54,12 @@ fn part1(input: &String) -> i32 {
     *coord_areas.iter().max().unwrap()
 }
 
-fn part2(input: &String) -> i32 {
-    let coordinates: Vec<(i32, i32)> = input.lines().map(|p| {
-        let p_vec: Vec<&str> = p.split(", ").collect();
-        (p_vec[0].parse().unwrap(), p_vec[1].parse().unwrap())
-    }).collect();
-
-    let (max_x, max_y) = coordinates.iter().fold((0, 0), |(max_x, max_y), (x, y)| (cmp::max(max_x, *x), cmp::max(max_y, *y)));
-    let (min_x, min_y) = coordinates.iter().fold((400, 400), |(min_x, min_y), (x, y)| (cmp::min(min_x, *x), cmp::min(min_y, *y)));
-
+fn part2(input: &String, threshold: i32) -> i32 {
+    let (coordinates, (max_x, max_y), (min_x, min_y)) = prepare(input);
     let mut region_area = 0;
+
     for x in min_x..max_x {
         for y in min_y..max_y {
-            let threshold = 10000; // 32 for test
             let distance_sum: i32 = coordinates.iter().map(|(cx, cy)| (x - cx).abs() + (y - cy).abs()).sum();
             if distance_sum < threshold {
                 region_area += 1;
@@ -82,5 +81,5 @@ fn validate_part1() {
 fn validate_part2() {
     let input = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9".to_string();
 
-    assert_eq!(16, part2(&input));
+    assert_eq!(16, part2(&input, 32));
 }
