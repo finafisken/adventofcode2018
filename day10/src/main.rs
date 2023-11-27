c46afa6a67e9541c39471748f2b132ede4fcdbdf
Markus Warne <markus.warne@gmail.com>
2023-11-27 19:35:21 +0100

    d10 part 1 done

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -8,8 +8,6 @@ struct Point {
     y: i32,
     velocity_x: i32,
     velocity_y: i32,
-    origin_x: i32,
-    origin_y: i32,
 }
 
 impl Point {
@@ -19,23 +17,27 @@ impl Point {
             y: num[1],
             velocity_x: num[2],
             velocity_y: num[3],
-            origin_x: num[0],
-            origin_y: num[1],
         }
     }
 
-    // fn pos_after(&self, steps: usize) -> Point {
-    //     Point {
-    //         x: self.origin_x + (self.velocity_x * steps as i32),
-    //         y: self.origin_y + (self.velocity_y * steps as i32),
-    //         ..*self
-    //     }
-    // }
+    fn tick(&mut self) {
+        self.x += self.velocity_x;
+        self.y += self.velocity_y;
+    }
+}
 
-    fn pos_after(&mut self, steps: usize) -> &Self {
-        self.x = self.origin_x + (self.velocity_x * steps as i32);
-        self.y = self.origin_y + (self.velocity_y * steps as i32);
-        self
+fn paint(points: Vec<Point>, max_x: i32, min_x: i32, max_y: i32, min_y: i32) {
+    for y in min_y..=max_y {
+        let mut row: Vec<char> = Vec::new();
+        for x in min_x..=max_x {
+            if points.iter().any(|p| p.x == x && p.y == y) {
+                row.push('#');
+            } else {
+                row.push('.');
+            }
+        }
+        let output_row: String = row.iter().cloned().collect();
+        println!("{output_row}");
     }
 }
 
@@ -46,18 +48,33 @@ fn main() {
 
     let regex = Regex::new(r"-?\d+").unwrap();
 
-    // let test = input.lines().collect::<Vec<&str>>();
+    let mut points = Vec::new();
     for line in input.lines() {
         let numbers: Vec<i32> = regex
             .find_iter(line)
             .filter_map(|s| s.as_str().parse().ok())
             .collect();
         let point = Point::new(numbers);
-        println!("{:?}", point);
+        points.push(point);
     }
-    // println!("{:?}", test);
-    // let input_numbers: Vec<u32> = input
-    // .split_whitespace()
-    // .filter_map(|s| s.parse().ok())
-    // .collect();
+
+    loop {
+        for point in &mut points {
+            point.tick();
+        }
+
+        let max_x = points.iter().map(|p| p.x).fold(i32::MIN, i32::max);
+        let min_x = points.iter().map(|p| p.x).fold(i32::MAX, i32::min);
+        let max_y = points.iter().map(|p| p.y).fold(i32::MIN, i32::max);
+        let min_y = points.iter().map(|p| p.y).fold(i32::MAX, i32::min);
+
+        let width = max_x.abs_diff(min_x);
+        let height = max_y.abs_diff(min_y);
+
+        if width <= 70 && height <= 30 {
+            // paint
+            paint(points, max_x, min_x, max_y, min_y);
+            break;
+        }
+    }
 }
