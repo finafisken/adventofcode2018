2fc868568cc18ffb113944985b42f81605c3dfeb
Markus Warne <markus.warne@gmail.com>
2023-12-02 00:12:57 +0100

    day12 getting close

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -3,46 +3,66 @@ use std::{collections::HashMap, fs::read_to_string};
 fn main() {
     let input = read_to_string("input.txt").unwrap();
     let initial_state = input.lines().next().unwrap().replace("initial state: ", "");
-    // println!("{}", initial_state);
 
     let mut match_results: HashMap<String, char> = HashMap::new();
 
     for line in input.lines().skip(2) {
-        let [pattern, _, result]: [&str; 3] = line
-            .split(' ')
+        let [pattern, result]: [&str; 2] = line
+            .split(" => ")
             .collect::<Vec<&str>>()
             .try_into()
             .expect("wrong format in input");
 
         match_results.insert(String::from(pattern), result.chars().next().unwrap());
-        // println!("{} to {}", pattern, result);
     }
 
     let mut state = initial_state.clone();
     for _gen in 1..=20 {
-        let mut new_state = String::default();
-        for chunk in state.chars().collect::<Vec<_>>().windows(5) {
-            let part = chunk.iter().collect::<String>();
-            if match_results.contains_key(&part) {
-                let new_status = match_results.get(&part).expect("OOOF");
-                let mut chars: Vec<char> = part.chars().collect();
-                chars[2] = *new_status;
+        let mut new_state = String::from("..");
+        new_state.push_str(state.as_str());
+        new_state.push_str("..");
+        let extended_state = new_state.clone();
 
-                new_state += &chars.iter().collect::<String>();
-                // println!("hit {}", new_status);
-            } else {
-                new_state += &part;
-                // println!("miss");
+        for i in 2..new_state.len() - 2 {
+            let to_match = &extended_state[i - 2..=i + 2];
+            if match_results.contains_key(to_match) {
+                new_state = new_state
+                    .chars()
+                    .enumerate()
+                    .fold(Vec::new(), |mut acc, (ii, char)| {
+                        if ii == i {
+                            acc.push(*match_results.get(to_match).unwrap())
+                        } else {
+                            acc.push(char)
+                        }
+                        acc
+                    })
+                    .into_iter()
+                    .collect::<String>();
             }
-            // println!("{}", chunk.iter().collect::<String>());
+            //  else {
+            //     new_state = new_state
+            //         .chars()
+            //         .enumerate()
+            //         .fold(Vec::new(), |mut acc, (ii, char)| {
+            //             if ii == i {
+            //                 acc.push('.')
+            //             } else {
+            //                 acc.push(char)
+            //             }
+            //             acc
+            //         })
+            //         .into_iter()
+            //         .collect::<String>();
+            // }
         }
 
-        println!("{}", new_state);
+        println!("gen {}: {}", _gen, new_state);
         state = new_state;
     }
-    let sum_pots = state.chars().enumerate().fold(0, |sum, (i, val)| {
+    let sum_pots: isize = state.chars().enumerate().fold(0, |sum, (i, val)| {
         if val == '#' {
-            return sum + i;
+            return sum + i as isize - 40;
         }
         sum
     });
